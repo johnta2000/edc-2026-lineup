38a1e8ee90d8d7297dabea741cd8500e25acc56b
John <46404373+johnta2000@users.noreply.github.com>
2026-05-02 23:08:50 -0700

    Serve compiled app from GitHub Pages root

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,21 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   base: "/edc-2026-lineup/",
+  build: {
+    rollupOptions: {
+      input: "index.dev.html",
+      output: {
+        assetFileNames: (assetInfo) => {
+          if (assetInfo.name?.endsWith(".css")) {
+            return "assets/index.css";
+          }
+
+          return "assets/[name][extname]";
+        },
+        chunkFileNames: "assets/[name].js",
+        entryFileNames: "assets/index.js",
+      },
+    },
+  },
   plugins: [react()],
 });
